02145fce4ada15bd71e295121bfeaac8479d08d7
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-24 19:27:53 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+// data.js
+
+const products = [
+  {
+    _id: 1,
+    name: 'Product 1',
+    image: '/images/product1.jpg', 
+    price: 29.99
+  },
+  {
+    _id: 2, 
+    name: 'Product 2',
+    image: '/images/product2.jpg',
+    price: 24.99
+  },
+]
+
+export default products
